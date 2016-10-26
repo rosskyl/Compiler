d2e2eeb3716da86304a09e71794ebe7ede16130f
rosskyl <rosskyl@mnstate.edu>
2016-10-26 09:39:54 -0500

    Wrote dummy eval methods for each node

--- nodes.cpp
+++ nodes.cpp
@@ -3,32 +3,40 @@
 
 
 
-void Node::eval() {
-	
-};
+void Node::eval() {};
 
 void ProgramNode::eval() {
-	
+	std::vector<Node*>::iterator i;
+	for (i = lines.begin(); i != lines.end(); i++)
+		(*i)->eval();
 };
 
 void IfNode::eval() {
-	
+	condition->eval();
+	ifCode->eval();
+	if (elseCode != NULL)
+		elseCode->eval();
 };
 
 void WhileNode::eval() {
-	
+	condition->eval();
+	code->eval();
 };
 
 void BlockNode::eval() {
-	
+	std::vector<Node*>::iterator i;
+	for (i = lines.begin(); i != lines.end(); i++)
+		(*i)->eval();
 };
 
 void BoolExpNode::eval() {
-	
+	lVal->eval();
+	rVal->eval();
 };
 
 void NumExpNode::eval() {
-	
+	lVal->eval();
+	rVal->eval();
 };
 
 void IntNode::eval() {
@@ -36,7 +44,7 @@ void IntNode::eval() {
 };
 
 void FloatNode::eval() {
-	
+	std::cout << "Float: " << val << std::endl;
 };
 
 
